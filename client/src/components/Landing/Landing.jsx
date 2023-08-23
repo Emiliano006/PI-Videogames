ecbab01053470614363d1faee646957a78da4235
Emiliano006 <emiliano.costabile@hotmail.com>
2023-08-23 14:35:17 -0300

    commit

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -7,6 +7,7 @@ const Landing = () => {
 
   return (
     <div className={styles.principal}>
+      <h1>Welcome to Game World</h1>
       <Link className={styles.buttonContainer} to="/home">START</Link>
       <div className={styles.imageContainer}>
         <img src={Fondo} alt="Imagen de la Landing Page" />
